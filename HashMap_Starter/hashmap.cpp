0b0fd583e1469c91033c2d3de8b76e837390d4dd
Peiye Liu <peiyeliu@usc.edu>
2023-01-01 21:55:20 -0800

    hashmap all tests passed

--- HashMap_Starter/hashmap.cpp
+++ HashMap_Starter/hashmap.cpp
@@ -201,6 +201,9 @@ void HashMap<K, M, H>::debug() const{
     std::cout << std::setw(30) << std::setfill('-') << '\n' << std::setfill(' ');
 }
 
+
+
+
 template <typename K, typename M, typename H>
 void HashMap<K, M, H>::rehash(size_t new_bucket_count) {
 if (new_bucket_count == 0) {
@@ -266,7 +269,9 @@ std::ostream& operator<<(std::ostream& os, const HashMap<K, M, H>& rhs) {
 
 // copy constructor
 template <typename K, typename M, typename H>
-HashMap<K, M, H>::HashMap (const HashMap& other){
+HashMap<K, M, H>::HashMap (const HashMap& other)
+    : _size(0), _hash_function(other._hash_function),_buckets_array(other._buckets_array.size())
+{
     for(auto [key, val]: other){insert({key, val});}
 }
 
@@ -281,16 +286,13 @@ HashMap<K, M, H>& HashMap<K, M, H>::operator=(const HashMap& other){
 
 // move constructor
 template <typename K, typename M, typename H>
-HashMap<K, M, H>::HashMap (HashMap&& other){
-    _size = std::move(other._size);
-    _hash_function = std::move(other._hash_function);
-    _buckets_array.resize(other.bucket_count());
+HashMap<K, M, H>::HashMap (HashMap&& other)
+    : _size(other._size), _hash_function(other._hash_function),_buckets_array(other._buckets_array.size())
+{
     for(long unsigned int i = 0; i < _buckets_array.size(); ++i){
-        _buckets_array[i] = nullptr;
         _buckets_array[i] = std::move(other._buckets_array[i]);
-        other._buckets_array[i] = nullptr;
     }
-    other._size = 0;
+    other._size=0;
 }
 
 // move assignment constructor
@@ -300,13 +302,10 @@ HashMap<K, M, H>& HashMap<K, M, H>::operator=(HashMap&& other){
     clear();
     _size = std::move(other._size);
     _hash_function = std::move(other._hash_function);
-    _buckets_array.resize(other.bucket_count());
+    _buckets_array.resize(other._buckets_array.size());
     for(long unsigned int i = 0; i < _buckets_array.size(); ++i){
-        _buckets_array[i] = nullptr;
         _buckets_array[i] = std::move(other._buckets_array[i]);
-        other._buckets_array[i] = nullptr;
     }
-    other._size = 0;
     return *this;
 }
 
